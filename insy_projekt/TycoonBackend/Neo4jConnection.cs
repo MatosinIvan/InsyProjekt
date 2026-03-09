39646f82510e2e37988faaa3ea8c4ee15c478336
MatosinIvan <167750113+MatosinIvan@users.noreply.github.com>
2026-03-09 14:26:15 +0100

    Add files via upload

---
+++ insy_projekt/TycoonBackend/Neo4jConnection.cs
@@ -0,0 +1,18 @@
+﻿using Neo4j.Driver;
+
+namespace TycoonBackend
+{
+    public class Neo4jConnection
+    {
+        public IDriver ConDriver { get; private set; }
+
+        public Neo4jConnection()
+        {
+            var uri = "neo4j://127.0.0.1:7687";
+            var user = "neo4j";
+            var password = "postgres";
+            ConDriver = GraphDatabase.Driver(uri, AuthTokens.Basic(user,password));
+        }
+        public IDriver GetDriver() { return ConDriver; }
+    }
+}
---
+++ insy_projekt/TycoonConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Neo4j.Driver;
+using TycoonBackend;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var connection = new Neo4jConnection();
+            var driver = connection.GetDriver();
+            await using var session = driver.AsyncSession();
+
+            var tmName = "testname";
+            var tmRobux = 2000;
+
+            var createQuery = "CREATE (p:Person {name:$name,robux:$robux})";
+            var parameters = new {name=tmName,robux=tmRobux};
+
+            await session.RunAsync(createQuery, parameters);
+            Console.WriteLine($"{tmName} added successfully");
+        }
+    }
+}
